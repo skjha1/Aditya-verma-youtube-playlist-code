60e161ff8a84e0b63d324077907ccdf68922bc8d
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-05-28 10:48:41 +0530

    Create 04 maximum of all subarray of size k.cpp

---
+++ Sliding Window/04 maximum of all subarray of size k.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+       vector<int> ans;
+        list<int> l;
+        int i=0;
+        int j=0;
+        
+        if (k>nums.size())
+        {
+            ans.push_back(*max_element(nums.begin(),nums.end()));
+            return ans;
+        }
+        
+        while (j<nums.size())
+        {
+            while(l.size()>0 && l.back() <nums[j])
+            {
+                l.pop_back();
+            }
+            l.push_back(nums[j]);
+            if ((j-i+1)<k)
+                j++;
+            if (j-i+1==k)
+            {
+                ans.push_back(l.front());
+                if (l.front()==nums[i])
+                    l.pop_front();
+                i++;
+                j++;
+            }
+            if (k>nums.size())
+                ans.push_back(*max_element(nums.begin(),nums.end()));
+            
+        }
+        return ans;
+    }
+};
