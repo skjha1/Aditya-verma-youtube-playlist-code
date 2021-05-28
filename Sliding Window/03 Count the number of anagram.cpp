a8b280048f46b6b67b3f52033497c26641429d25
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-05-28 10:23:33 +0530

    Create 03 Count the number of anagram.cpp

---
+++ Sliding Window/03 Count the number of anagram.cpp
@@ -0,0 +1,42 @@
+class Solution{
+  public:
+  
+  int search(string pat, string txt) {
+  unordered_map <char, int=""> m;
+  for(auto i: pat)
+  m[i]++;
+
+  int k = pat.size();
+  int count = m.size();
+  int ans = 0;
+  int i = 0, j = 0;
+
+  while(j < txt.size()) {
+
+  if(m.find(txt[j]) != m.end()) {
+  m[txt[j]]--;
+
+  if(m[txt[j]] == 0)
+  count--;
+  }
+
+  if(j - i + 1 < k) j++;
+
+  else if(j - i + 1 == k) {
+  if(count == 0)
+  ans++;
+
+  if(m.find(txt[i]) != m.end()) {
+  m[txt[i]]++;
+
+  if(m[txt[i]] == 1)
+  count++;
+  }
+
+  i++; j++;
+  }
+  }
+
+  return ans;
+  }
+};
