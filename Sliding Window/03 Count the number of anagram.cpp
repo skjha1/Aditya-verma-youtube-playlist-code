1bd62b2d3ad9f02fd813007c91b2cf466bfc0570
Deepak Patil <85918329+cruze0210@users.noreply.github.com>
2022-06-13 19:26:28 +0530

    Update 03 Count the number of anagram.cpp
    We can also solve the given problem using string sliding window method. just as shown.

--- Sliding Window/03 Count the number of anagram.cpp
+++ Sliding Window/03 Count the number of anagram.cpp
@@ -1,42 +1,36 @@
-class Solution{
-  public:
-  
-  int search(string pat, string txt) {
-  unordered_map <char, int=""> m;
-  for(auto i: pat)
-  m[i]++;
 
-  int k = pat.size();
-  int count = m.size();
-  int ans = 0;
-  int i = 0, j = 0;
 
-  while(j < txt.size()) {
+// WITHOUT MAP 
 
-  if(m.find(txt[j]) != m.end()) {
-  m[txt[j]]--;
+#include<bits/stdc++.h>
+using namespace std ;
+int main()
+{   
+    string s,p;
+    cin>>s>>p ;
+    int count=0 ;
+    string c="" ;
+    sort(p.begin(),p.end()) ;
+    int k=p.length();
+    int n=s.length() ;
+    // -----------------------------------
+    int i=0,j=0 ;
+     while(j<n)
+    {   c.push_back(s[j]) ;
+        if(j-i+1<k)
+        j++ ;
+        else if(j-i+1==k)
+        {   string cp=c;
+            sort(cp.begin(),cp.end());
+            if(cp==p)
+            count++ ;
+            c.erase(0,1);
+            i++;
+            j++;
+        }
 
-  if(m[txt[j]] == 0)
-  count--;
-  }
-
-  if(j - i + 1 < k) j++;
-
-  else if(j - i + 1 == k) {
-  if(count == 0)
-  ans++;
-
-  if(m.find(txt[i]) != m.end()) {
-  m[txt[i]]++;
-
-  if(m[txt[i]] == 1)
-  count++;
-  }
-
-  i++; j++;
-  }
-  }
-
-  return ans;
-  }
-};
+    }
+cout<<count ;
+// ----------------------------------------------------  
+    return 0;
+}
